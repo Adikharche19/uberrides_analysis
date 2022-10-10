64c6762eef98401036e157930537bb1f88c371ba
Adikharche19 <adikharche19@gmail.com>
2022-10-10 00:27:02 -0400

    addding new file for uber ride analysis

---
+++ uberrides.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import matplotlib.pyplot as pyplot
+import numpy as np
+# Loading the data from csv file 
+pd.read_csv('/Users/sachinkharche/uberrides_analysis/Data/uber-raw-data-apr14.csv')
