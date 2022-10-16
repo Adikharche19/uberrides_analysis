851e66361760491fff741942336c7fe1478fce92
Adikharche19 <adikharche19@gmail.com>
2022-10-15 23:09:36 -0400

    adding jyupter notebook

--- uberrides.py
+++ uberrides.py
@@ -2,4 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as pyplot
 import numpy as np
 # Loading the data from csv file 
-pd.read_csv('/Users/sachinkharche/uberrides_analysis/Data/uber-raw-data-apr14.csv')
+uberride = pd.read_csv('/Users/sachinkharche/uberrides_analysis/Data/uber-raw-data-apr14.csv',encoding='utf-8')
+uberride.head(2)
+uberride.shape
+uberride.duplicated().sum()
